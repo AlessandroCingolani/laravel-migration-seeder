a4583b1a15cdf47d988ef829278ad4fbf687f777
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-21 15:01:29 +0100

    [ add ] use models and function index

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $trains = Train::all();
+        if (empty($trains)) {
+            abort(404);
+        }
+        return view("home", compact("trains"));
+    }
 }
