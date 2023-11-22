f4644a5c975f0d0327be0c80aea938351c099f82
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-22 14:09:04 +0100

    [ change ] data format dateTime

--- database/migrations/2023_11_21_130015_create_trains_table.php
+++ database/migrations/2023_11_21_130015_create_trains_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('agency', 100);
             $table->string('departure_station', 100);
             $table->string('arrival_station', 100);
-            $table->time('departure_time');
-            $table->time('arrival_time');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
             $table->string('train_code', 15);
             $table->unsignedTinyInteger('number_carriage');
             $table->boolean('is_in_time')->default(true);
