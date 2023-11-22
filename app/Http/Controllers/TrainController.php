c7b7eb67f6366fbc94a7804745d47bee7fa7da03
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-22 10:04:02 +0100

    [ change ] condition stamp date

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -10,7 +10,7 @@ class TrainController extends Controller
 {
     public function index()
     {
-        $trains = Train::whereDate('date', Carbon::today())->get();
+        $trains = Train::where('date', '>=', Carbon::today())->get();
         if (empty($trains)) {
             abort(404);
         }
